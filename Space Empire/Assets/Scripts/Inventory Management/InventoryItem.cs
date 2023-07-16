7aa196d36c146180549d90d9cbc5a0679fe9c8ba
Barış Karakaya <baybarisk@outlook.com>
2023-07-16 18:05:10 +0300

    Inventory System added new scene

---
+++ Space Empire/Assets/Scripts/Inventory Management/InventoryItem.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Image image;
+    public TextMeshProUGUI countText;
+    [Header("UI")]
+    [HideInInspector] public Item item;
+    [HideInInspector] public int count;
+    [HideInInspector] public Transform parentAfterDrag;
+
+    public void InitialiseItem(Item newItem)
+    {
+        item = newItem;
+        image.sprite = newItem.image;
+        RefreshCount();
+    }
+
+    public void RefreshCount()
+    {
+        countText.text = count.ToString();
+        bool textActive = count > 1;
+        countText.gameObject.SetActive(textActive);
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) 
+    {
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
+    }
+}
---
+++ Space Empire/Assets/Scripts/Inventory Management/Item.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Scriptable Object/Item")]
+public class Item : ScriptableObject
+{
+    [Header("Only Gameplay")]
+    public TileBase tile;
+    public ItemType type;
+    public ActionType actionType;
+    public Vector2Int range = new Vector2Int(5,4);
+
+    [Header("Only UI")]
+    public bool stackable = true;
+    [Header("Both")]
+    public Sprite image;
+    public enum ItemType
+    {
+        Throwable,
+        Gun
+    }
+
+    public enum ActionType
+    {
+        Throw,
+        Fire
+    }
+}
